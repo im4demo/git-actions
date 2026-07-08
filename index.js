433138e5773d818ac54d0e5d37ad055d294a2f7e
imran4login <imran4login@gmail.com>
2026-07-08 12:46:40 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+// const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT ?? 8080;
+
+app.get('/', (req, res) => {
+    return res.json( { msg: 'Hello from server!!!' } );
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on porttt ${PORT}`);
+})
+
+
