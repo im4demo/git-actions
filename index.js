aaedc1cc654ceef60d81db8f2314c155cfcb6acf
imran4login <imran4login@gmail.com>
2026-07-08 13:20:13 +0300

    Modify

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
-// const PORT = process.env.PORT || 8080;
-const PORT = process.env.PORT ?? 8080;
+// const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT ?? 80;
 
 app.get('/', (req, res) => {
     return res.json( { msg: 'Hello from server!!!' } );
