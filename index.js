66354a1f93f33cf275d9edcd9627dd6143668ba4
imran4login <imran4login@gmail.com>
2026-07-08 23:44:11 +0300

    Upd

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 const PORT = process.env.PORT ?? 8080;
 
 app.get('/', (req, res) => {
-    return res.json( { msg: 'Hello from server---' } );
+    return res.json( { msg: 'Hello from server-UPD' } );
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is up and running on port --- ${PORT}`);
+    console.log(`Server is up and running on port --- UPD ${PORT}`);
 })
 
 
